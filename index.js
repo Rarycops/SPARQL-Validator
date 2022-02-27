1ad48aba2d6ed8f3c73e0db5431cc246cd60c076
Miguel <mj.rarycops@gmail.com>
2022-02-27 20:12:14 +0100

    patch

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ function main(){
         const time = (new Date()).toTimeString();
         core.setOutput("time", time);
 
-        // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
-        console.log(`The event payload: ${payload}`);
 
         console.log(files);
     }
