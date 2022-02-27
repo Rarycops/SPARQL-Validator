70dcc337fb0aca5868278d56359f6cdad1100b93
Miguel <mj.rarycops@gmail.com>
2022-02-27 19:41:27 +0100

    created pipeline for testing

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ function main(){
         // take the input to the action
         const file = core.getInput('file');
         
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+
+        // Get the JSON webhook payload for the event that triggered the workflow
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
+
         // check if the file is a .sparql file
         for (const file of changedFiles) {
             // get the exteension of the file
             const fileExtension = file.filename.split('.').pop();
 
             if (fileExtension == 'sparql'){
-
+                console.log(file.filename);
+            }
+            else{
+                console.log(file.filename, " is not a .sparql file");
             }
         }
     }
